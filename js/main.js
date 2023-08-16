b237f44e677d9ab4d85541feb01593b2048c2cb3
Renato Parraguez Rubilar <rena_010@hotmail.com>
2023-08-16 03:01:15 -0400

    casi finalizado

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ let pasoActual = document.getElementById(pasoActualID);
 let pasoCounter = 1;
 let seccion = "";
 let cuadro = "cuadro";
+let imagenAgregado = false;
+let file; 
 const btnEnmarcaciones = document.getElementById("btnEnmarcaciones");
 
 let cuadroEspec = {
@@ -18,8 +20,12 @@ let cuadroEspec = {
   color: '',
   paspartu: '',
   anchoPaspartu: '',
+  paspartuDigital: '',
   tipoPaspartu: '',
-  cantidad: 0
+  cantidad: 0,
+  colorImpresion: '',
+  nombreImg: '',
+  urlImg: '',
 }
 
 let totalProd = [];
@@ -33,83 +39,147 @@ let productoId = 0;
 //     $("#div3").fadeIn(3000);
 // });
 
-$(document).ready(() => {
-  $(".enmarcaciones1").hide();
+// $(document).ready(() => {
+//   // $(".enmarcaciones1").hide();
+//   let localTotalProd =  JSON.parse(localStorage.getItem("totalProd"));
+//   if(localTotalProd){
+//     totalProd = localTotalProd
+//     $(".cantCarro").html(totalProd.length);
+//     for(let i = 0; i < localTotalProd.length; i++){
+//       console.log(localTotalProd[i]);
+//       agregarProductoLocalStorage(localTotalProd[i])
+//     }
+//   }
 
-  btnEnmarcaciones.addEventListener("click", function () {
-    $(".servicio").hide();
-    cuadroEspec.tipo = 'Enmarcación';
-    seccion = "enmarcaciones";
-    cuadro = cuadro + "Enmarcaciones";
-    console.log(seccion + pasoCounter);
-    $("." + seccion + pasoCounter).show();
-    pasoCounter++;
-    // Esto se activa cuando se vuelve agregar un producto
-    $("." + seccion + pasoCounter).show();
-    $(`li:contains("${pasoCounter}")`).removeClass("step-prox");
-    $(`li:contains("${pasoCounter}")`).addClass("step-actual");
-  });
+// });
 
-  $(".volver").click(() => {
-    if (pasoCounter == 2) {
-      $(`li:contains("${pasoCounter}")`).removeClass("step-actual");
-      $(`li:contains("${pasoCounter}")`).addClass("step-prox");
-      pasoCounter--;
-      $("." + seccion + pasoCounter).hide();
-      $(".servicio").show();
-      console.log(seccion + pasoCounter);
-    } else {
-      $(`li:contains("${pasoCounter}")`).removeClass("step-actual");
-      $(`li:contains("${pasoCounter}")`).addClass("step-prox");
-      $("." + seccion + pasoCounter).hide();
-      $("." + cuadro + pasoCounter).hide();
-      pasoCounter--;
-      $("." + seccion + pasoCounter).show();
-    }
-  });
+function agregarProductoLocalStorage(producto){
+  let html = `<div class="card-producto mb-10" id="${producto.id}">
+                <div class="flex g-40">
+                  <div class="img-final imgProductoCarro" class="imageContainer">
+                    <img class="enCarroImg uploadedImage${producto.id}" src="assets/svg/cuadroNegroBlanco.svg" alt="Uploaded Image">
+                  </div>  
+                  <div>
+                    <div class="tw-700 f-16">${producto.tipo}</div>
+                    <div class="tw-400 f-16">Especificaciones de cuadro</div>          
+                    <div class="tw-600 f-16">Tamaño: <span class="tw-400">Ancho ${producto.largo} cm - Largo ${producto.ancho} cm</span></div>          
+                    <div class="tw-600 f-16">Modelo de marco: <span class="tw-400">${producto.modelo}</span></div>          
+                    <div class="tw-600 f-16">Paspartu: <span class="tw-400">${producto.paspartu}</span></div>          
+                    <div class="tw-600 f-16 text-center">...</div>
+                    <div class="flex f-align-items-center f-justify-between">
+                      <div class="mt-5 flex f-align-items-center g-40 mr-50">
+                        <div class="operacion pointer" onclick="operacion('-')">-</div>
+                        <div class="detalle-cantidad tw-600 f-16">${producto.cantidad}</div>
+                        <div class="operacion pointer" onclick="operacion('+')">+</div>
+                      </div>
+                      <div class="tw-600 f-16 text-underline pointer" onclick="deleteProd(${producto.id});">Eliminar</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              `
+  $("#productos").append(html);
 
-  $(".siguiente").click(() => {
-    $("." + seccion + pasoCounter).hide();
-    pasoCounter++;
-    console.log(seccion + pasoCounter);
-    $("." + seccion + pasoCounter).show();
-    $("." + cuadro + pasoCounter).show();
-    console.log(cuadro+pasoCounter);
-    $(`li:contains("${pasoCounter}")`).removeClass("step-prox");
-    $(`li:contains("${pasoCounter}")`).addClass("step-actual");
-  });
+  if(producto.urlImg != ''){
+    var uploadedImage = $('.uploadedImage'+cuadroEspec.id);
+    var imageContainer = $('.imageContainer');
 
-  $(".largo").on('blur', function () {
-    $(".valorLargo").html($(this).val() + " cm");
+    // Establecer la fuente (src) de la imagen usando la URL creada
+    uploadedImage.attr('src', producto.urlImg);
+    imageContainer.css('display', 'block');
+  }
 
-  });
-  $(".ancho").on('blur', function () {
-    $(".valorAncho").html($(this).val() + " cm");
-  });
+}
 
-
-  let colorEnmarcacion = "";
-  let botonPrevio = null;
-
-  $(".btnColor").click(function () {
-    let textoBoton = $(this).text().trim();
-    if (colorEnmarcacion !== "") {
-      if (botonPrevio !== null) {
-        botonPrevio.removeClass("btnColorBorder");
-      }
-      botonPrevio = $(".btnColor").filter(function () {
-        return $(this).text().trim() === colorEnmarcacion;
-      });
-      botonPrevio.removeClass("btnColorBorder");
-    }
-    // Asignamos el botón actual como el nuevo botón previo
-    botonPrevio = $(this);
-    $(this).addClass("btnColorBorder");
-    colorEnmarcacion = textoBoton;
-  });
-
+btnEnmarcaciones.addEventListener("click", function () {
+  $(".servicio").hide();
+  cuadroEspec.tipo = 'Enmarcación';
+  seccion = "enmarcaciones";
+  cuadro = cuadro + "Enmarcaciones";
+  $("." + seccion + pasoCounter).show();
+  console.log("antes: " + pasoCounter);
+  pasoCounter++;
+  // Esto se activa cuando se vuelve agregar un producto
+  $("." + seccion + pasoCounter).show();
+  console.log(seccion + pasoCounter);
+  $(`li:contains("${pasoCounter}")`).removeClass("step-prox");
+  $(`li:contains("${pasoCounter}")`).addClass("step-actual");
 });
 
+// Esto es seccion IMPRESION + ENMARCACIONES
+$("#btnImpresiones").click(function(){
+  $(".servicio").hide();
+  cuadroEspec.tipo = "Enmarcación + Impresion";
+  seccion = "impresiones";
+  cuadro = cuadro + "Impresiones";
+  console.log("impresion antes: " + pasoCounter);
+  $("." + seccion + pasoCounter).show();
+  pasoCounter++;
+  console.log("impresion despues: " + pasoCounter);
+  $("."+ seccion + pasoCounter).show();
+  $(`li:contains("${pasoCounter}")`).removeClass("step-prox");
+  $(`li:contains("${pasoCounter}")`).addClass("step-actual");
+});
+
+$(".volver").click(() => {
+  console.log("inicio volver: " + pasoCounter);
+  if (pasoCounter == 1) {
+    // $(`li:contains("${pasoCounter}")`).removeClass("step-actual");
+    // $(`li:contains("${pasoCounter}")`).addClass("step-prox");
+    // pasoCounter--;
+    $("." + seccion + pasoCounter).hide();
+    $(".servicio").show();
+  } else {
+    $(`li:contains("${pasoCounter}")`).removeClass("step-actual");
+    $(`li:contains("${pasoCounter}")`).addClass("step-prox");
+    $("." + seccion + pasoCounter).hide();
+    $("." + cuadro + pasoCounter).hide();
+    pasoCounter--;
+    $("." + seccion + pasoCounter).show();
+  }
+  console.log("paso Actual: " + pasoCounter)
+});
+
+$(".siguiente").click(() => {
+  $("." + seccion + pasoCounter).hide();
+  pasoCounter++;
+  console.log(seccion + pasoCounter);
+  $("." + seccion + pasoCounter).show();
+  $("." + cuadro + pasoCounter).show();
+  console.log(cuadro+pasoCounter);
+  $(`li:contains("${pasoCounter}")`).removeClass("step-prox");
+  $(`li:contains("${pasoCounter}")`).addClass("step-actual");
+  console.log("paso actual: " + pasoCounter);
+});
+
+$(".largo").on('blur', function () {
+  $(".valorLargo").html($(this).val() + " cm");
+
+});
+$(".ancho").on('blur', function () {
+  $(".valorAncho").html($(this).val() + " cm");
+});
+
+
+let colorEnmarcacion = "";
+let botonPrevio = null;
+
+$(".btnColor").click(function () {
+  let textoBoton = $(this).text().trim();
+  if (colorEnmarcacion !== "") {
+    if (botonPrevio !== null) {
+      botonPrevio.removeClass("btnColorBorder");
+    }
+    botonPrevio = $(".btnColor").filter(function () {
+      return $(this).text().trim() === colorEnmarcacion;
+    });
+    botonPrevio.removeClass("btnColorBorder");
+  }
+  // Asignamos el botón actual como el nuevo botón previo
+  botonPrevio = $(this);
+  $(this).addClass("btnColorBorder");
+  colorEnmarcacion = textoBoton;
+});
 
 $("input[name='radioAnchoPas']").change(function () {
   if ($(this).is(":checked")) {
@@ -117,6 +187,14 @@ $("input[name='radioAnchoPas']").change(function () {
     $(".valorPassport").html($(this).val() + " cm")
   }
 
+})
+
+$("input[name='imp-radioPas']").change(function () {
+  if($(this).val() == "Digital"){
+    $(".pas-digital").show();
+  }else{
+    $(".pas-digital").hide();
+  }
 })
 
 function showModal(modal) {
@@ -139,19 +217,44 @@ function operacion(simbolo) {
 
 
 function agregarProducto() {
-  cuadroEspec.id = productoId;
-  cuadroEspec.orientacion = $("input[name='orientacion']:checked").val();
-  cuadroEspec.largo = $("#largo").val();
-  cuadroEspec.ancho = $("#ancho").val();
-  cuadroEspec.modelo = $("input[name='orientacion']:checked").val();
-  cuadroEspec.color = $(".btnColorBorder").html();
-  cuadroEspec.paspartu = $("input[name='radioPas']:checked").val();
-  cuadroEspec.anchoPaspartu = $("input[name='radioAnchoPas']:checked").val();
-  cuadroEspec.tipoPaspartu = $("input[name='radioTipo']:checked").val();
-  cuadroEspec.cantidad = 1;
-  totalProd.push(cuadroEspec);
+  if(seccion == 'impresiones'){
+    cuadroEspec.id = productoId;
+    cuadroEspec.colorImpresion = $("input[name='color-impresion']:checked").val();
+    cuadroEspec.orientacion = $("input[name='orientacion']:checked").val();
+    cuadroEspec.largo = $("#largo").val();
+    cuadroEspec.ancho = $("#ancho").val();
+    cuadroEspec.modelo = $("input[name='modelo-impresion']:checked").val();
+    cuadroEspec.color = $(".btnColorBorder").html();
+    cuadroEspec.paspartu = $("input[name='imp-radioPas']:checked").val();
+    cuadroEspec.paspartuDigital = $("input[name='pasDigital']:checked").val();
+    cuadroEspec.anchoPaspartu = $("input[name='imp-radioAnchoPas']:checked").val();
+    cuadroEspec.tipoPaspartu = $("input[name='imp-radioTipo']:checked").val();
+    cuadroEspec.cantidad = 1;
+    productoId++;
 
-  productoId++;
+  }else if (seccion == 'enmarcaciones'){
+    cuadroEspec.id = productoId;
+    cuadroEspec.orientacion = $("input[name='orientacion']:checked").val();
+    cuadroEspec.largo = $("#largo").val();
+    cuadroEspec.ancho = $("#ancho").val();
+    cuadroEspec.modelo = $("input[name='modelo']:checked").val();
+    cuadroEspec.color = $(".btnColorBorder").html();
+    cuadroEspec.paspartu = $("input[name='radioPas']:checked").val();
+    cuadroEspec.anchoPaspartu = $("input[name='radioAnchoPas']:checked").val();
+    cuadroEspec.tipoPaspartu = $("input[name='radioTipo']:checked").val();
+    cuadroEspec.cantidad = 1;
+    productoId++;
+  }
+  
+   totalProd.push(cuadroEspec);
+   localStorage.setItem("totalProd", JSON.stringify(totalProd));
+  
+  $(".cantCarro").html(totalProd.length);
+  agregarProductoHTML();
+}
+
+
+function agregarProductoHTML(){  
 
   $(".detalle-tipo").html(cuadroEspec.tipo);
   $(".detalle-tamaño").html(`Ancho ${cuadroEspec.largo} cm - Largo ${cuadroEspec.ancho} cm`);
@@ -162,9 +265,11 @@ function agregarProducto() {
   $(".detalle-anchoPaspartu").html(cuadroEspec.anchoPaspartu);
   $(".detalle-cantidad").html(cuadroEspec.cantidad)
 
-  let html = `<div class="card-producto" id="${cuadroEspec.id}">
+  let html = `<div class="card-producto mb-10" id="${cuadroEspec.id}">
                 <div class="flex g-40">
-                  <div><img src="assets/svg/imgProducto.svg" alt=""></div>
+                  <div class="img-final imgProductoCarro" class="imageContainer">
+                    <img class="enCarroImg uploadedImage${cuadroEspec.id}" src="assets/svg/cuadroNegroBlanco.svg" alt="Uploaded Image">
+                  </div>  
                   <div>
                     <div class="tw-700 f-16">${cuadroEspec.tipo}</div>
                     <div class="tw-400 f-16">Especificaciones de cuadro</div>          
@@ -173,7 +278,7 @@ function agregarProducto() {
                     <div class="tw-600 f-16">Paspartu: <span class="tw-400">${cuadroEspec.paspartu}</span></div>          
                     <div class="tw-600 f-16 text-center">...</div>
                     <div class="flex f-align-items-center f-justify-between">
-                      <div class="mt-5 flex f-align-items-center g-40">
+                      <div class="mt-5 flex f-align-items-center g-40 mr-50">
                         <div class="operacion pointer" onclick="operacion('-')">-</div>
                         <div class="detalle-cantidad tw-600 f-16">${cuadroEspec.cantidad}</div>
                         <div class="operacion pointer" onclick="operacion('+')">+</div>
@@ -185,7 +290,14 @@ function agregarProducto() {
               </div>
             `
   $("#productos").append(html);
-  
+
+  if(imagenAgregado){
+    var imageURL = URL.createObjectURL(file);
+    var uploadedImage = $('.uploadedImage'+cuadroEspec.id);
+    var imageContainer = $('.imageContainer');
+    uploadedImage.attr('src', imageURL);
+    imageContainer.css('display', 'block');
+  }
 }
 
 function inicio() {
@@ -194,7 +306,6 @@ function inicio() {
   $("." + seccion + pasoCounter).hide();
   $(".servicio").show();
 
- 
   for(let i = 1; i <= pasoCounter; i++){
     $("." + cuadro + i).hide();
     if (i > 1){
@@ -228,8 +339,15 @@ function inicio() {
     paspartu: '',
     anchoPaspartu: '',
     tipoPaspartu: '',
-    cantidad: 0
+    cantidad: 0,
+    urlImg : ''
+
   }
+
+  var uploadedImage = $('.uploadedImage');
+  uploadedImage.attr('src', "assets/svg/cuadroNegroBlanco.svg");
+
+  imagenAgregado = false;
 }
 
 function verCarrito(){
@@ -240,9 +358,34 @@ function verCarrito(){
   $(".carrito").show();
 }
 
-
 function deleteProd(ProducID){
+  totalProd = totalProd.filter((item) => item.id !== ProducID)
+  // totalProd.splice(ProducID,1);
   $(`#${ProducID}`).remove();
-  totalProd.slice(ProducID,1);
-
+  localStorage.setItem("totalProd", JSON.stringify(totalProd));
+  console.log("totalProd: ");
+  console.log(totalProd);
+  $(".cantCarro").html(totalProd.length);
 }
+
+$('#uploadInput').on('change', function (event) {
+    imagenAgregado = true;  
+    var uploadedImage = $('.uploadedImage');
+    var imageContainer = $('.imageContainer');
+
+    file = event.target.files[0];
+
+    if (file) {
+        var imageURL = URL.createObjectURL(file);
+        console.log("imageURL: " + imageURL);
+        cuadroEspec.urlImg = imageURL 
+        uploadedImage.attr('src', imageURL);
+        imageContainer.css('display', 'block');
+    }
+
+    var filename = $(this).val().split("\\").pop();
+    cuadroEspec.nombreImg = filename;
+    $(".nombreImagen").html(filename);
+
+
+});
